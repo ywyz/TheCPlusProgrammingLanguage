96508078c730c2d65c47626a9a1c8d18e5095a8b
ywyz <admin@ywyz.tech>
2020-06-27 21:39:53 +0800

    finish Chapter 02

---
+++ Chapter02/myfirst/myfirst.cpp
@@ -0,0 +1,11 @@
+﻿// myfirst.cpp
+
+#include <iostream>
+int main()
+{
+	using namespace std;
+	cout << "Come up and C++ me some time.";
+	cout << endl;
+	cout << "You won't regret it!" << endl;
+	return 0;
+}
